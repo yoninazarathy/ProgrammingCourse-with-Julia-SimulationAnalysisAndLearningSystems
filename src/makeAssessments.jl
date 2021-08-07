e34c398c620e2df954808311cebb92e6c75907a8
Yoni Nazarathy <yoninazarathy@gmail.com>
2021-08-07 15:11:11 +1000

    hw2

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -6,7 +6,7 @@ kwargs = (doctype = "md2html", out_path = "assessment_html", template = "math250
 
 assesmentSubfolder = "2021Assessment"
 
-weave("markdown/$(assesmentSubfolder)/hw1.jmd"; kwargs...)
+# weave("markdown/$(assesmentSubfolder)/hw1.jmd"; kwargs...)
 weave("markdown/$(assesmentSubfolder)/hw2.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
