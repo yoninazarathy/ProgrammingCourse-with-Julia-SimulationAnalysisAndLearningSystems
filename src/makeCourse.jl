7ba49c3e5b9bf3e902943f2af912b9e07f87d31e
yoninazarathy <yoninazarathy@gmail.com>
2021-08-25 16:56:15 +1000

    unit4

--- src/makeCourse.jl
+++ src/makeCourse.jl
@@ -8,10 +8,11 @@ kwargs_uc = (doctype = "md2html", out_path = "lectures_html", template = "math25
 kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "math2504.tpl", mod = Main) 
 
 
-# weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
-# weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
-# weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
 weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-5.jmd"; kwargs_uc...) 
 # weave("markdown/lecture-unit-6.jmd"; kwargs_uc...) 
 # weave("markdown/lecture-unit-7.jmd"; kwargs_uc...) 
+
