0defd82c7d21e82ba3383707fb5e2c03d37fd57d
yoninazarathy <yoninazarathy@gmail.com>
2021-10-05 14:39:28 +1000

    project 2

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -8,6 +8,6 @@ assesmentSubfolder = "2021Assessment"
 
 # weave("markdown/$(assesmentSubfolder)/hw1.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/hw2.jmd"; kwargs...)
-weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
+# weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
 weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project3.jmd"; kwargs...)#
