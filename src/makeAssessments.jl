5521e8a0043a80fe197383d14c342a531435ef31
yoninazarathy <yoninazarathy@gmail.com>
2022-06-14 12:22:21 +1000

    prepare for S2 2022

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -4,10 +4,9 @@ cd("..")
 
 kwargs = (doctype = "md2html", out_path = "assessment_html", template = "math2504assessment.tpl")
 
-assesmentSubfolder = "2021Assessment"
+assesmentSubfolder = "2022Assessment"
 
-# weave("markdown/$(assesmentSubfolder)/hw1.jmd"; kwargs...)
-# weave("markdown/$(assesmentSubfolder)/hw2.jmd"; kwargs...)
-# weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
-# weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
+weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
+weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
+weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
 weave("markdown/$(assesmentSubfolder)/project3.jmd"; kwargs...)#
