7cf1eafc497216c53cd229f3173db1591b2f15c9
yoninazarathy <yoninazarathy@gmail.com>
2022-07-29 20:11:51 +1000

    updates to BigHW

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -10,3 +10,21 @@ weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project3.jmd"; kwargs...)#
+
+
+### This is a temporary hack to add links to question numbers (it assumes questions are heading 2's)
+q = 0
+lines = readlines("assessment_html/bighw.html"; keep=true)
+
+for (i, line) in enumerate(lines)
+    if contains(line, "<h2>")
+        global q = q + 1
+        lines[i] = replace(line, "<h2>" => "<h2 id=q$q>")
+    end
+end
+
+open("assessment_html/bighw.html", "w") do f
+    for line in lines
+        write(f, line)
+    end
+end
