70a462f13ed33ed58702f4c398c4d945709b6be5
Andy Ferris <ferris.andy@gmail.com>
2022-10-02 13:15:53 +1000

    Workaround to make compilation work

--- src/makeCourse.jl
+++ src/makeCourse.jl
@@ -1,8 +1,8 @@
+ENV["GKSwstype"]="nul" # workaround for https://github.com/JunoLab/Weave.jl/issues/436
 using Weave
 cd(@__DIR__)
 using Pkg; Pkg.activate("..")
 cd("..")
-
 
 kwargs_uc = (doctype = "md2html", out_path = "lectures_html", template = "math2504_under_const.tpl", mod = Main) #for "under construction
 kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "math2504.tpl", mod = Main) 
@@ -12,8 +12,8 @@ kwargs_publish_b = (doctype = "md2html", out_path = "lectures_html", mod = Main)
 # weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
-weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
+# weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-5.jmd"; kwargs_publish...) 
-# weave("markdown/lecture-unit-6.jmd"; kwargs_publish...) 
+weave("markdown/lecture-unit-6.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-7.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-8.jmd"; kwargs_publish...) 
