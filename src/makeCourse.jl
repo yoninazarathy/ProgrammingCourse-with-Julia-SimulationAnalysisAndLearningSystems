cc5e870c9db3cad0899866234556f33a60e75210
yoninazarathy <yoninazarathy@gmail.com>
2023-10-25 16:30:57 +1000

    week 13

--- src/makeCourse.jl
+++ src/makeCourse.jl
@@ -3,6 +3,7 @@ using Weave
 cd(@__DIR__)
 using Pkg; Pkg.activate("..")
 cd("..")
+@show pwd()
 
 kwargs_uc = (doctype = "md2html", out_path = "lectures_html", template = "math2504_under_const.tpl", mod = Main) #for "under construction
 kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "math2504.tpl", mod = Main) 
@@ -14,6 +15,6 @@ kwargs_publish_b = (doctype = "md2html", out_path = "lectures_html", mod = Main)
 # weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-5.jmd"; kwargs_publish...) 
-weave("markdown/lecture-unit-6.jmd"; kwargs_publish...) 
+# weave("markdown/lecture-unit-6.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-7.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-8.jmd"; kwargs_publish...) ;
