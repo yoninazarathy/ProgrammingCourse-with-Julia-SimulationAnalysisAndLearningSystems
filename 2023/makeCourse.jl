a9e8f58bceb43f661e2ec5511892b152b798ed7c
yoninazarathy <yoninazarathy@gmail.com>
2024-06-28 23:24:57 +1000

    adjusting lecture numbers

--- 2023/makeCourse.jl
+++ 2023/makeCourse.jl
@@ -9,8 +9,7 @@ kwargs_uc = (doctype = "md2html", out_path = "lectures_html", template = "math25
 kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "math2504.tpl", mod = Main) 
 kwargs_publish_b = (doctype = "md2html", out_path = "lectures_html", mod = Main) 
 
-
-# weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
