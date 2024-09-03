a13c1d8424647908c9ab51bc5ba2589053b95206
yoninazarathy <yoninazarathy@gmail.com>
2024-09-03 10:59:06 +1000

    unit 6 and project 1

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -10,8 +10,8 @@ kwargs = (doctype = "md2html", out_path = "assessment_html", template = "math250
 
 assesmentSubfolder = "2024Assessment"
 
-weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
-# weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
+# weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
+weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project3.jmd"; kwargs...)#
 
