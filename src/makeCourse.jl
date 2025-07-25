fdb5a69adbe0887a107f3c82f8740745d37412e2
dietmaroelz@gmail.com <dietmaroelz@gmail.com>
2025-07-25 15:59:25 +1000

    updated BigHW for 2025

--- src/makeCourse.jl
+++ src/makeCourse.jl
@@ -10,11 +10,11 @@ kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "m
 kwargs_publish_b = (doctype = "md2html", out_path = "lectures_html", mod = Main) 
 
 
-# weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-5.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-6.jmd"; kwargs_publish...) 
-weave("markdown/lecture-unit-7.jmd"; kwargs_publish...) 
+# weave("markdown/lecture-unit-7.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-8.jmd"; kwargs_publish...) ;
