77bf8d3e2a14c148d7e9e136b268704659b216b0
Andy Ferris <ferris.andy@gmail.com>
2025-08-17 17:00:47 +1000

    Unit 2 updates

--- src/makeCourse.jl
+++ src/makeCourse.jl
@@ -10,8 +10,8 @@ kwargs_publish = (doctype = "md2html", out_path = "lectures_html", template = "m
 kwargs_publish_b = (doctype = "md2html", out_path = "lectures_html", mod = Main) 
 
 
-weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
-# weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
+# weave("markdown/lecture-unit-1.jmd"; kwargs_publish...)
+weave("markdown/lecture-unit-2.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-3.jmd"; kwargs_publish...)
 # weave("markdown/lecture-unit-4.jmd"; kwargs_publish...) 
 # weave("markdown/lecture-unit-5.jmd"; kwargs_publish...) 
