5df7b0243e2a690d30cc68bf6fdcd1810e4954a1
dietmaroelz@gmail.com <dietmaroelz@gmail.com>
2025-09-04 15:39:39 +1000

    modified project1

--- src/makeAssessments.jl
+++ src/makeAssessments.jl
@@ -10,8 +10,8 @@ kwargs = (doctype = "md2html", out_path = "assessment_html", template = "math250
 
 assesmentSubfolder = "2025Assessment"
 
-weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
-# weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
+# weave("markdown/$(assesmentSubfolder)/bighw.jmd"; kwargs...)
+weave("markdown/$(assesmentSubfolder)/project1.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project2.jmd"; kwargs...)
 # weave("markdown/$(assesmentSubfolder)/project3.jmd"; kwargs...)#
 
